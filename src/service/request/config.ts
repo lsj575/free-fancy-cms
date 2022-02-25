3fa73487dca09fd4bdc36b93a7e1ee8c0a785c28
sijie_long <768712778@qq.com>
2022-02-25 17:03:11 +0800

    feat: 添加服务器环境下的后端ip

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -4,7 +4,7 @@ const TIME_OUT = 10000
 if (process.env.NODE_ENV === 'development') {
   BASE_URL = 'http://114.132.199.181:8081'
 } else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = ''
+  BASE_URL = 'http://114.132.199.181:8081'
 } else {
   BASE_URL = ''
 }
