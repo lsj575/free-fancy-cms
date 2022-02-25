c76b2152b8e4828ad75e8dbc4a9a8afc30214fee
sijie_long <768712778@qq.com>
2022-02-25 16:56:03 +0800

    feat: 新增对用户类型的转换插槽

--- src/components/content-card/content-card.vue
+++ src/components/content-card/content-card.vue
@@ -2,8 +2,8 @@
   <div class="content-card">
     <fancy-table
       :propList="contentTableConfig.tablePropList"
-      :dataList="userList"
-      :dataCount="userCount"
+      :dataList="list"
+      :dataCount="count"
       :title="contentTableConfig.title"
     >
       <!-- 1. header 中的插槽 -->
@@ -12,15 +12,18 @@
       </template>
 
       <!-- 2. 表格中的插槽 -->
+      <template #userType="scope">
+        {{ userTypeName[scope.row.userType] }}
+      </template>
       <template #status="scope">
-        <el-button size="mini" :style="scope.row.status === 1 ? 'success' : 'danger'">{{
-          scope.row.status === 1 ? '正常' : '禁用'
+        <el-button size="mini" :type="scope.row.status === 0 ? 'success' : 'danger'" plain>{{
+          scope.row.status === 0 ? '正常' : '禁用'
         }}</el-button>
       </template>
       <template #heandler>
         <div class="heandle-btns">
-          <el-button type="text" size="mini">编辑</el-button>
-          <el-button type="text" size="mini">删除</el-button>
+          <el-button :icon="Edit" type="text" size="mini">编辑</el-button>
+          <el-button :icon="Delete" type="text" size="mini">删除</el-button>
         </div>
       </template>
     </fancy-table>
@@ -29,8 +32,9 @@
 <script lang="ts">
 import { defineComponent, computed } from 'vue'
 import { useStore } from '../../store'
-import systemModule from '../../store/main/system/system'
 import fancyTable from '../fancy-table/fancy-table.vue'
+import { Edit, Delete } from '@element-plus/icons'
+
 export default defineComponent({
   props: {
     contentTableConfig: {
@@ -46,17 +50,28 @@ export default defineComponent({
     fancyTable
   },
   setup(props) {
+    const userTypeName = ['系统管理员', '测试员', '普通用户']
     const store = useStore()
-    store.dispatch('system/getDataList', {
+    store.dispatch('systemModule/getDataList', {
       pageName: props.pageName,
       queryInfo: {
-        offset: 0,
+        isAsc: true,
+        sortColumns: ['gmt_create'],
+        current: 1,
         size: 10
       }
     })
 
-    const userList = computed(() => store.getters['systemModule/pageListData'](props.pageName))
-    const userCount = computed(() => store.getters['systemModule/pageCountData'](props.pageName))
+    const list = computed(() => store.getters['systemModule/pageListData'](props.pageName))
+    const count = computed(() => store.getters['systemModule/pageCountData'](props.pageName))
+
+    return {
+      list,
+      count,
+      userTypeName,
+      Edit,
+      Delete
+    }
   }
 })
 </script>
