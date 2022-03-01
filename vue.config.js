82d1dbeabc1d25fe5dc3e03a5a66a0fac0f530c1
sijie_long <768712778@qq.com>
2022-03-01 14:59:45 +0800

    feat: 修改标签栏图标

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  pwa: {
+    iconPaths: {
+      favicon32: 'logo.png',
+      favicon16: 'logo.png',
+      appleTouchIcon: 'logo.png',
+      maskIcon: 'logo.png',
+      msTileImage: 'logo.png'
+    }
+  }
+}
