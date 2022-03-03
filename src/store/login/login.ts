ad8b0da212360b51bd3409f8e6155698ef270deb
sijie_long <768712778@qq.com>
2022-03-03 16:03:33 +0800

    fix: 修改刷新token定时器未在登录后设置的问题

--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -1,5 +1,6 @@
 import { Module } from 'vuex'
 import router from '@/router'
+import { ElMessage } from 'element-plus'
 
 import { LoginState } from './types'
 import { RootState } from '../types'
@@ -13,11 +14,11 @@ import {
 } from '@/service/login/login'
 import { Account } from '@/service/login/types'
 import localCache from '@/utils/cache'
-import { mapMenusToRoutes } from '@/utils/map-menus'
+import { mapMenusToPermissions, mapMenusToRoutes } from '@/utils/map-menus'
 
 // 定时任务：每隔一段时间刷新token
 // 为了保证后台的安全性
-let refreshTokenTimer: NodeJS.Timer
+let refreshTokenTimer: number | null = null
 
 const loginModule: Module<LoginState, RootState> = {
   namespaced: true,
@@ -25,7 +26,8 @@ const loginModule: Module<LoginState, RootState> = {
     return {
       token: '',
       userInfo: {},
-      userMenus: []
+      userMenus: [],
+      permissions: []
     }
   },
   mutations: {
@@ -44,6 +46,10 @@ const loginModule: Module<LoginState, RootState> = {
       routes.forEach((route) => {
         router.addRoute('main', route)
       })
+
+      // 获取用户按钮的权限
+      const permissions = mapMenusToPermissions(userMenus)
+      state.permissions = permissions
     }
   },
   actions: {
@@ -51,9 +57,18 @@ const loginModule: Module<LoginState, RootState> = {
       const captchaResult = await getCaptcha()
       return captchaResult
     },
-    async accountLoginAction({ commit }, payload: Account) {
+    async accountLoginAction({ dispatch, commit }, payload: Account) {
       // 1. 登录
       const loginResult = await accountLoginRequest(payload)
+      const code = loginResult.code
+      if (code === 1000) {
+        ElMessage({
+          message: '登录成功',
+          type: 'success'
+        })
+      } else {
+        ElMessage.error(loginResult.msg)
+      }
       const { user, Authorization } = loginResult.data
       commit('changeToken', Authorization)
       localCache.setCache('token', Authorization)
@@ -70,7 +85,16 @@ const loginModule: Module<LoginState, RootState> = {
       commit('changeUserMenus', userMenus)
       localCache.setCache('userMenus', userMenus)
 
-      // 4. 跳转到首页
+      // 4. 设置刷新token的定时器
+      if (refreshTokenTimer !== null) {
+        clearInterval(refreshTokenTimer)
+        refreshTokenTimer = null
+      }
+      refreshTokenTimer = window.setInterval(() => {
+        dispatch('refreshTokenAction')
+      }, 1000 * 60 * 25)
+
+      // 5. 跳转到首页
       router.push('/main')
     },
     accountLogoutAction({ commit }) {
@@ -87,7 +111,10 @@ const loginModule: Module<LoginState, RootState> = {
       localCache.removeCache('userMenus')
 
       // 4. 删除更新token的timer
-      clearInterval(refreshTokenTimer)
+      if (refreshTokenTimer !== null) {
+        clearInterval(refreshTokenTimer)
+        refreshTokenTimer = null
+      }
 
       // 4. 跳转到登录页面
       router.push('/login')
@@ -103,6 +130,15 @@ const loginModule: Module<LoginState, RootState> = {
       if (token) {
         commit('changeToken', token)
         dispatch('refreshTokenAction')
+
+        // 重设定时器
+        if (refreshTokenTimer !== null) {
+          clearInterval(refreshTokenTimer)
+          refreshTokenTimer = null
+        }
+        refreshTokenTimer = window.setInterval(() => {
+          dispatch('refreshTokenAction')
+        }, 1000 * 60 * 25)
       }
 
       const userInfo = localCache.getCache('userInfo')
